f61a372283010b9168b093cc9871b8d9ae62cc5d
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2020-11-27 07:13:15 +0000

    Use parameterized query in students.js
    implemented to avoid sql injection.

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,13 +7,17 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const queryString = `
 SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort 
 FROM students
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+const values = [`%${cohortName}%`, limit];
+pool.query(queryString, values)
 .then(res => {
   //console.log(res.rows);
   res.rows.forEach(user => {
