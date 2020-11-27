64c8df46f2a4e357dc76244c5db6c06cd7fb9f1c
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2020-11-27 07:19:47 +0000

    Use parameterized input for queries
    implemented to avoid sequel injection in teachers.js

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,15 +7,19 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-  SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
-  FROM teachers
-  JOIN assistance_requests ON teachers.id = teacher_id
-  JOIN students ON student_id = students.id 
-  JOIN cohorts ON cohort_id = cohorts.id 
-  WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-  ORDER BY teacher;
-`)
+const queryString = `
+SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+FROM teachers
+JOIN assistance_requests ON teachers.id = teacher_id
+JOIN students ON student_id = students.id 
+JOIN cohorts ON cohort_id = cohorts.id 
+WHERE cohorts.name = $1
+ORDER BY teacher;
+`;
+const cohort = process.argv[2] || 'JUL02';
+const values = [cohort];
+
+pool.query(queryString, values)
 .then(res => {
   console.log('query successful!');
   res.rows.forEach(row => {
